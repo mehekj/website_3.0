badcfa04c2d0995320cc4a1fd1364f86b5da16f8
mehekj <mehek.jethani@gmail.com>
2025-07-29 23:20:57 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Poppins } from "next/font/google";
 const poppins = Poppins({
 	weight: ["300", "500", "700"],
 	variable: "--font-poppins",
+	subsets: ["latin"],
 });
 
 export default function RootLayout({
