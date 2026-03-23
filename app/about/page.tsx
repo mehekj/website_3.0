ff461c0f0a70a7ede646037dcf151c54faa3c57f
mehekj <mehek.jethani@gmail.com>
2026-03-22 22:45:42 -0700

    fix punctuation

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,19 +11,20 @@ export default function About() {
 					className="object-cover scale-150"
 				/>
 			</div>
-			<h1 className="font-bold text-5xl">Hello! I'm Mehek</h1>
+			<h1 className="font-bold text-5xl">Hello! I&apos;m Mehek</h1>
 			<p className="my-4">
-				I'm a 24 year old software engineer currently working at Infinity Ward
-				on the Call of Duty franchise. At IW, I'm on the rendering team and I
-				mostly work on writing C++ code for the cross-platform game engine, but
-				I also occasionally get to touch pipeline, tools, automation, and UI.
+				I&apos;m a 24 year old software engineer currently working at Infinity
+				Ward on the Call of Duty franchise. At IW, I&apos;m on the rendering
+				team and I mostly work on writing C++ code for the cross-platform game
+				engine, but I also occasionally get to touch pipeline, tools,
+				automation, and UI.
 			</p>
 			<p className="my-4">
-				Though I'm now located in Los Angeles, I originally grew up in New
-				Jersey before moving to Providence, RI where I completed my bachelor's
-				degree in computer science at Brown University. At Brown, I was a TA for
-				CS1230: Intro To Computer Graphics and a member of the Dash research
-				group under Professor Andy van Dam.
+				Though I&apos;m now located in Los Angeles, I originally grew up in New
+				Jersey before moving to Providence, RI where I completed my
+				bachelor&apos;s degree in computer science at Brown University. At
+				Brown, I was a TA for CS1230: Intro To Computer Graphics and a member of
+				the Dash research group under Professor Andy van Dam.
 			</p>
 			<p className="my-4">
 				Aside from coding and playing video games, you can usually find me
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,8 @@ export default function Projects() {
 		<div className="max-w-6xl m-auto px-8 py-24">
 			<h1 className="font-bold text-5xl">Sometimes I code things</h1>
 			<p className="my-4">
-				here's some projects I've actually finished (there's many more)
+				here&apos;s some projects I&apos;ve actually finished (there&apos;s many
+				more)
 			</p>
 		</div>
 	);
