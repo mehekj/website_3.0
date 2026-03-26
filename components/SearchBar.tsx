de089610ace54b24b2ab324a28427c7d358ae2b6
mehekj <mehek.jethani@gmail.com>
2026-03-25 23:42:40 -0700

    minor

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,7 +5,7 @@ import { Search } from "react-feather";
 export default function SearchBar() {
 	return (
 		<div className="my-8 relative">
-			<div className="flex justify-between items-center group border-1 my-1 hover:translate-[-2px] hover:shadow-[5px_5px] hover:border-main shadow-black dark:shadow-white hover:shadow-main transition duration-200 overflow-hidden focus-within:shadow-[5px_5px] focus-within:translate-[-2px] w-full">
+			<div className="flex justify-between items-center group border-1 my-4 hover:translate-[-2px] hover:shadow-[5px_5px] hover:border-main shadow-black dark:shadow-white hover:shadow-main transition duration-200 overflow-hidden focus-within:shadow-[5px_5px] focus-within:translate-[-2px] w-full">
 				<input
 					className="outline-none w-full px-4 py-2"
 					placeholder="Search..."
