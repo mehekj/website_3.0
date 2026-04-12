679fd0d093657969cf7aebdf325360605a48a824
mehekj <mehek.jethani@gmail.com>
2026-04-11 23:09:24 -0700

    hide test pages and text styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,21 +15,21 @@ export default function About() {
 			<h1 className="font-bold text-4xl md:text-5xl mb-4 order-first">
 				Hello! I&apos;m Mehek
 			</h1>
-			<p className="my-4">
+			<p className="my-4 text-justify">
 				I&apos;m a 24 year old software engineer currently working at Infinity
 				Ward on the Call of Duty franchise. At IW, I&apos;m on the rendering
 				team and I mostly work on writing C++ code for the cross-platform game
 				engine, but I also occasionally get to touch pipeline, tools,
 				automation, and UI.
 			</p>
-			<p className="my-4">
+			<p className="my-4 text-justify">
 				Though I&apos;m now located in Los Angeles, I originally grew up in New
 				Jersey before moving to Providence, RI where I completed my
 				bachelor&apos;s degree in computer science at Brown University. At
 				Brown, I was a TA for CS1230: Intro To Computer Graphics and a member of
 				the Dash research group under Professor Andy van Dam.
 			</p>
-			<p className="my-4">
+			<p className="my-4 text-justify">
 				Aside from coding and playing video games, you can usually find me
 				watching too much TV, board gaming, reading, picking up assorted arts
 				and crafts projects, and playing the violin poorly.
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 	return (
 		<div className="max-w-6xl grid place-items-center m-auto px-8">
 			<div>
-				<h1 className="text-7xl font-black tracking-tighter">
+				<h1 className="text-7xl font-black tracking-tighter text-center">
 					COOL LANDING PAGE
 				</h1>
 				<p className="text-center">coming soon maybe</p>
